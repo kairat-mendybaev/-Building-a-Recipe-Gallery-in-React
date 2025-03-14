ce051a634c0a92499eef49145ec5994483aa756b
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2025-03-13 23:45:10 -0700

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RecipeGallery from './RecipeGallery';
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeGallery />
+    </div>
+  );
+}
+
+export default App;
